4d0a505447e2bd09dd8f4c8c822d9739f98836db
knchandra <chandrashekar.kn@3icube.com>
2013-11-21 17:41:50 +0530

    Updated Create billing

--- actitime-framework/src/test/java/settings/CreateBilling.java
+++ actitime-framework/src/test/java/settings/CreateBilling.java
@@ -7,6 +7,8 @@ import testbase.BaseClass;
 public class CreateBilling extends BaseClass{
   @Test
   public void testCreateBilling() {	  
-	  System.out.println("Create Billing");
+	  System.out.println("Start create billing");
+	  //create billing code goes here
+	  System.out.println("End create billing");
   }
 }
